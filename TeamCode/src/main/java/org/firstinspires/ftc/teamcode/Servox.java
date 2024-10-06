245ba8d7fd744730f73858fd676c8872544bd791
Pranpran3 <pranavleaps@gmail.com>
2024-10-05 17:12:03 -0700

    servos!

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/Servox.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/Servox.java
@@ -8,20 +8,14 @@ import com.qualcomm.robotcore.hardware.Servo;
 
 @TeleOp (name="ServoPran")
 public class Servox extends LinearOpMode {
-   Servo servo1;
+    Servo servo1;
     @Override
     public void runOpMode() throws InterruptedException {
         servo1 = hardwareMap.get(Servo.class, "servo1");
         waitForStart();
         while (opModeIsActive()){
-            while (gamepad1.a) {
+            if (gamepad1.a) {
                 servo1.setPosition(1);
-            }
-            while (gamepad1.b){
-                servo1.setPosition(-1);
-            }
-            while (gamepad1.atRest()){
-                servo1.setPosition(0);
             }
         }
     }
