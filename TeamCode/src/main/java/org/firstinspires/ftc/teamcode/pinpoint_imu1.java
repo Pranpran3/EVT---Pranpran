df8bb0f84e75aeda5104af29d6a76bec5a1fc98e
Pranpran3 <pranavleaps@gmail.com>
2024-12-25 17:14:46 -0800

    odometry stuff

---
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/pinpoint_imu1.java
@@ -0,0 +1,28 @@
+package org.firstinspires.ftc.teamcode;
+
+
+import com.qualcomm.robotcore.eventloop.opmode.Autonomous;
+import com.qualcomm.robotcore.eventloop.opmode.LinearOpMode;
+import com.qualcomm.robotcore.eventloop.opmode.TeleOp;
+import com.qualcomm.robotcore.eventloop.opmode.OpMode;
+import com.qualcomm.robotcore.hardware.DcMotor;
+
+import org.firstinspires.ftc.robotcore.external.navigation.AngleUnit;
+import org.firstinspires.ftc.robotcore.external.navigation.DistanceUnit;
+import org.firstinspires.ftc.robotcore.external.navigation.Pose2D;
+
+@Autonomous(name="pinpoint_imu1", group="Linear OpMode")
+
+public class pinpoint_imu1 extends LinearOpMode {
+
+    double frontLeft, frontRight, backLeft, backRight;
+
+    GoBildaPinpointDriver odo;
+
+
+
+
+
+
+
+}
